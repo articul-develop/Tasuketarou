407621e7a972c8b97c8fad32edec4b02706b8be6
Hirose <c_hirose@articul.co.jp>
2025-02-06 16:56:45 +0900

    不要ファイル削除

--- DeleteFile/js/API.js
+++
@@ -1,7 +0,0 @@
-// API.js
-window.API_CONFIG = {
-  ENDPOINT: 'https://kintone2-37613.bubbleapps.io/version-test/api/1.1/wf/kintone-authenticate',
-  AUTH_TOKEN: 'Bearer 22b9eea99705018da6d43ac7ed0392e1',
-  ItemKey: '3' // ★BubbleでのアプリID
-  
-};
