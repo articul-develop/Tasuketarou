35097d9c6975d1a9cd8fc20e4041bdff6a121dbc
Hirose <c_hirose@articul.co.jp>
2025-02-14 10:54:55 +0900

    Tableテスト_非表示修正完了

--- TabletoAPP/js/desktop_main.js
+++ TabletoAPP/js/desktop_main.js
@@ -65,9 +65,7 @@
     ];
     kintone.events.on(eventsToShow, function (event) {
       const config = kintone.plugin.app.getConfig(PLUGIN_ID) || {};
-      console.log('キー項目非表示：',config.hideKeyField);
-      if (config.hideKeyField === 'true') {
-        console.log('キー項目非表示がtrueのため非表示にします');        
+      if (config.hideKeyField === 'true') {      
         kintone.app.record.setFieldShown(ROW_IDENTIFIER_FIELD, false);
       }
       return event;
