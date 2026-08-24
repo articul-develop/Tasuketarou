1071670a692355d8488e49d221a27be5cd73e0d1
Hirose <c_hirose@articul.co.jp>
2026-08-24 12:59:51 +0900

    Update openmailer (auto-generated by build-main-live)

--- openmailer/js/config.js
+++ openmailer/js/config.js
@@ -742,10 +742,7 @@
       return;
     }
 
-    kintone.plugin.app.setConfig(newConfig, () => {
-      alert('設定を保存しました。');
-      window.location.href = `/k/admin/app/${kintone.app.getId()}/plugin/`;
-    });
+    kintone.plugin.app.setConfig(newConfig);
   });
 
   cancelBtn.addEventListener('click', () => {
